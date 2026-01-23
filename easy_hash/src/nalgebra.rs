a81c5123d591e514af13538d28c92a8ffac228d1
brendan c <brendan@colloran.net>
2026-01-23 13:31:42 -0800

    add impl for nalgabra isometry

--- easy_hash/src/nalgebra.rs
+++ easy_hash/src/nalgebra.rs
@@ -1,6 +1,6 @@
-use nalgebra::{Const, OPoint, UnitVector2};
+use nalgebra::{Complex, Const, Isometry, OPoint, Unit, UnitVector2};
 
-use crate::{type_salt, EasyHash};
+use crate::{EasyHash, type_salt};
 
 impl EasyHash for nalgebra::Vector2<f32> {
     const TYPE_SALT: u32 = type_salt::<Self>();
@@ -39,6 +39,21 @@ impl EasyHash for OPoint<f32, Const<2>> {
     fn ehash(&self) -> u64 {
         let mut checksum = fletcher::Fletcher64::new();
         checksum.update(&[Self::TYPE_SALT, self.x.to_bits(), self.y.to_bits()]);
+        checksum.value()
+    }
+}
+
+impl EasyHash for Isometry<f32, Unit<Complex<f32>>, 2> {
+    const TYPE_SALT: u32 = type_salt::<Self>();
+    fn ehash(&self) -> u64 {
+        let mut checksum = fletcher::Fletcher64::new();
+        checksum.update(&[
+            Self::TYPE_SALT,
+            self.translation.x.to_bits(),
+            self.translation.y.to_bits(),
+            self.rotation.re.to_bits(),
+            self.rotation.im.to_bits(),
+        ]);
         checksum.value()
     }
 }
--- easy_hash/tests/test_nalgebra.rs
+++ easy_hash/tests/test_nalgebra.rs
@@ -1,5 +1,5 @@
 use easy_hash::EasyHash;
-use nalgebra::{Vector2, Vector3, UnitVector2, Point2};
+use nalgebra::{Isometry2, Point2, UnitVector2, Vector2, Vector3};
 
 #[test]
 fn test_vector2_hash() {
@@ -35,4 +35,16 @@ fn test_point2_hash() {
     let c: Point2<f32> = Point2::new(2.0, 3.0);
     assert_eq!(a.ehash(), b.ehash());
     assert_ne!(a.ehash(), c.ehash());
+}
+
+#[test]
+fn test_isometry2_hash() {
+    let a: Isometry2<f32> = Isometry2::new(Vector2::new(1.0, 2.0), 0.0);
+    let b: Isometry2<f32> = Isometry2::new(Vector2::new(1.0, 2.0), 0.0);
+    let c: Isometry2<f32> = Isometry2::new(Vector2::new(2.0, 3.0), 0.0);
+    let d: Isometry2<f32> = Isometry2::new(Vector2::new(1.0, 2.0), std::f32::consts::PI / 4.0);
+
+    assert_eq!(a.ehash(), b.ehash());
+    assert_ne!(a.ehash(), c.ehash());
+    assert_ne!(a.ehash(), d.ehash());
 }
